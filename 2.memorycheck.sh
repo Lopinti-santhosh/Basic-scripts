1d26a8bfee8aeb6edcb069fbb8069e8e2517a256
santhosh <smileysanthosh29@gmail.com>
2026-06-28 17:10:57 +0530

    delete older logs

--- 2.memorycheck.sh
+++ 2.memorycheck.sh
@@ -7,8 +7,8 @@ while IFS= read -r line
 do 
      usage=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
      folder=$( echo $line |awk -F " " '{print $NF}' )
-    if [  $disk_usage -ge $disk_threshold  ]
+if [  $disk_usage -ge $disk_threshold  ]
 
     echo" $folder is  is more then of $disk_threshold ,courrent usage is $usage "
-
+fi 
 done <<< "$disk_usage"
