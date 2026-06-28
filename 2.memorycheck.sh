1b97246fff2513dce87241dfa3938799368031c2
santhosh <smileysanthosh29@gmail.com>
2026-06-28 17:02:13 +0530

    delete older logs

---
+++ 2.memorycheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+disk_usage=$(df -hT | grep xfs)
+disk_threshold=5
+
+while IFS = read -r line
+do 
+     usage=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+     folder=$( echo $line |awk -F " " '{print $NF}' )
+    if [  $disk_usage -ge $disk_threshold  ]
+       echo(" $folder is  is more then of $disk_threshold ,courrent usage is $usage ")
+
+done >>> $disk_usage
