5bf3ca63f5b32d2c31ccc72f639a28b2bee99cc6
santhosh <smileysanthosh29@gmail.com>
2026-06-28 15:50:51 +0530

    delete older logs

--- 1.deleteoldlogs.sh
+++ 1.deleteoldlogs.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-folder=/tmp/applogss
+#!/bin/bash
 
+SOURCE_DIRECTORY=/tmp/app-logs
 
-if [ -d $folder ]
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
 then
-   echo "folder is exits"
+    echo -e "$G Source directory exists $N"
 else
-   echo "folder is not exits"
-exit 1
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+    exit 1
 fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do
+    echo -e "$Y Deleting file: $line $N"
+    rm -rf $line
+done <<< $FILES
  
