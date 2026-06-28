eca26b0881cf4dd09271e1044afdaeb606eccdad
santhosh <smileysanthosh29@gmail.com>
2026-06-28 15:34:22 +0530

    delete older logs

--- 1.deleteoldlogs.sh
+++ 1.deleteoldlogs.sh
@@ -4,8 +4,8 @@ folder=/tmp/applogss
 
 if [ -d $folder ]
 then
-   echo ("folder is exits")
+   echo("folder is exits")
 else
-   echo ( "folder is not exits")
+   echo("folder is not exits")
 exit 1
 fi
