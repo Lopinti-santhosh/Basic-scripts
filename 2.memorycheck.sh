a4b93eab43c431f485b57ed01103d06a5be4848d
santhosh <smileysanthosh29@gmail.com>
2026-06-28 17:13:05 +0530

    delete older logs

--- 2.memorycheck.sh
+++ 2.memorycheck.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-disk_usage=$(df -hT | grep xfs)
-disk_threshold=5
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+MESSAGE=""
 
 while IFS= read -r line
-do 
-     usage=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-     folder=$( echo $line |awk -F " " '{print $NF}' )
-if [  $disk_usage -ge $disk_threshold  ]
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
+    fi
+done <<< $DISK_USAGE
 
-    echo" $folder is  is more then of $disk_threshold ,courrent usage is $usage "
 
-done <<< "$disk_usage"
+
+# echo "body" | mail -s "subject" to-address
